0b985519b5fb0114b297ddf27b80806b62780416
George Noseevich <ngo@lvk.cs.msu.su>
2016-06-14 23:31:00 +0300

    Fixed tz for utc environments

--- gmaps_route_get_time.py
+++ gmaps_route_get_time.py
@@ -1,25 +1,30 @@
 from __future__ import print_function
 from datetime import datetime
 import sys
+import pytz
 
 import googlemaps
 
 gmaps = googlemaps.Client(key=open('gooma.key', 'r').read().strip())
 
-now = datetime.now()
 directions = gmaps.directions(
 	sys.argv[1],
 	sys.argv[2],
 	mode="driving",
-	departure_time=now,
+	departure_time='now',
         avoid=['tolls']
 )[0]
+
+now = datetime.utcnow().replace(tzinfo = pytz.utc)
+tz = pytz.timezone('Europe/Moscow')
+now = now.astimezone(tz)
 
 duration = 0.0
 distance = 0.0
 for leg in directions['legs']:
     duration += leg['duration_in_traffic']['value']
     distance += leg['distance']['value']
+
 
 print(
         '''%d.%d,%s,%d,%d,"%s",%d,%f''' %(
