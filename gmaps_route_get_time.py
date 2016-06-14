d7418a262e15cdc645efd022230394eb7e78663b
George Noseevich <ngo@lvk.cs.msu.su>
2016-06-14 23:14:49 +0300

    MVP

---
+++ gmaps_route_get_time.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import sys
+
+import googlemaps
+
+gmaps = googlemaps.Client(key=open('gooma.key', 'r').read().strip())
+
+now = datetime.now()
+directions = gmaps.directions(
+	sys.argv[1],
+	sys.argv[2],
+	mode="driving",
+	departure_time=now,
+        avoid=['tolls']
+)[0]
+
+duration = 0.0
+distance = 0.0
+for leg in directions['legs']:
+    duration += leg['duration_in_traffic']['value']
+    distance += leg['distance']['value']
+
+print(
+        '''%d.%d,%s,%d,%d,"%s",%d,%f''' %(
+            now.day, now.month, now.strftime('%A'),now.hour, now.minute,
+            directions['summary'], int(duration / 60), distance / 1000
+        )
+)
